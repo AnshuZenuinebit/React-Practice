12c9a6e7958f457d1ea05a37aa5278fc8b3cfa4a
AnshuZenuinebit <135975525+AnshuZenuinebit@users.noreply.github.com>
2023-07-12 22:14:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+class App extends Component {
+  render() {
+
+   // }
+//}
+
+//const App = () => {
+  return (
+    <div className='App'>
+        <Greet/>
+        <Welcome/>
+      
+    </div>
+  );
+}
+}
+
+export default App
